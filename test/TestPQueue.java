9b4eb848ec61b63815a18615417ede509875de5a
Yang Liu <shenxing0426@gmail.com>
2025-10-23 19:23:35 +1000

    Test file issue solved

--- test/TestPQueue.java
+++ test/TestPQueue.java
@@ -2,6 +2,7 @@
  * Supplied by the COMP3506/7505 teaching team, Semester 2, 2025.
  */
 
+import uq.comp3506.a2.structures.Entry;
 import uq.comp3506.a2.structures.Heap;
 
 public class TestPQueue {
@@ -10,7 +11,17 @@ public class TestPQueue {
         System.out.println("Testing the Heap-based Priority Queue Class...");
      
         Heap<Integer, String> pq = new Heap<>();
-        // Your code here
+
+        // test 1: basic insertion and peek
+        System.out.println("Test 1: basic insertion and peek ");
+        pq.insert(5, "Five");
+        pq.insert(3, "Three");
+        pq.insert(7, "Seven");
+        Entry<Integer, String> min = pq.peekMin();
+        System.out.println("Peek min: Key=" + min.getKey() + ", Value=" + min.getValue());
+
+
+
 
     }
 }
